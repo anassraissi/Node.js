49094969f6f2a781f231ab5454f6d7de7e306def
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-12-17 12:52:58 +0100

    require and module expemple read fucntion from page to page

---
+++ public/app.js
@@ -0,0 +1,3 @@
+var counter=require('./count');
+//jbad ga3 les fonction ffichier count js naf smitha tema 3awed declariha zkhdam biha
+console.log(counter['anass','Raissi','Achraf']);
---
+++ public/count.js
@@ -0,0 +1,8 @@
+var counter=function(array){
+    console.log('this array has '+array.length+' itmes ');
+}
+// counter(['Raissi','Anass','marouane']);
+
+module.exports=counter;  
+// kharaj had fonction counter 3la bra bach tasta3mal tema
+ 
