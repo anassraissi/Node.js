2698afd4665545d8fcbcedc20978d16f21b752e8
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-12-25 15:21:24 +0100

    writable Streams

--- public/server/readable_stream.js
+++ public/server/readable_stream.js
@@ -1,10 +1,17 @@
 var http=require('http');
 var fs=require('fs');
         // 9ra fichier wafichih fconsole
-var MyReadStream=fs.createReadStream(__dirname +'/file_to_read.txt','utf-8');
-MyReadStream.on('data',function(chunk){   //mnin tji data siftha l console log waafuchiha
-        console.log('new chunk recieved');
-        console.log(chunk);
+var MyReadStream=fs.createReadStream(__dirname +'/file_to_read.txt','utf-8');  // create read stream 
+var MyWriteStream=fs.createWriteStream(__dirname +'/read_file.txt');    // create write stream
+
+MyReadStream.on('data',function(chunk){   //mnin tji data 3amar fichier read_file
+var MyWriteStream=fs.createWriteStream(__dirname +'/read_file.txt');
+        console.log('fill the file read_file.txt ');
+
+        MyWriteStream.write(chunk);
+
 })
+
+
 
 
