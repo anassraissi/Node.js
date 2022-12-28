52518569c3ab2186fa8e871af7cb4c1661903610
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-12-28 20:30:46 +0100

    using Stream redable.pipe in local in server

--- public/server/readable_stream.js
+++ public/server/readable_stream.js
@@ -4,13 +4,28 @@ var fs=require('fs');
 var MyReadStream=fs.createReadStream(__dirname +'/file_to_read.txt','utf-8');  // create read stream 
 var MyWriteStream=fs.createWriteStream(__dirname +'/read_file.txt');    // create write stream
 
-MyReadStream.on('data',function(chunk){   //mnin tji data 3amar fichier read_file
-var MyWriteStream=fs.createWriteStream(__dirname +'/read_file.txt');
-        console.log('fill the file read_file.txt ');
 
-        MyWriteStream.write(chunk);
+// instead of sending data by the method 
 
-})
+// MyReadStream.on('data',function(chunk){   //mnin tji data 3amar fichier read_file
+//         var MyWriteStream=fs.createWriteStream(__dirname +'/read_file.txt');
+//                 console.log('fill the file read_file.txt ');
+        
+//                 MyWriteStream.write(chunk);  // ktab data chank
+        
+//         })
+        
+// we can so esely
+
+MyReadStream.pipe(MyWriteStream);  //  pipe has a perpus send from readstrem to writestream.
+
+
+
+
+
+
+
+
 
 
 
