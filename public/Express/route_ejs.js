19c61ac7823fdda1ffc03c63bb977b7d22992d8c
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-02-15 21:09:38 +0100

    mideware static file merge css in server

--- public/Express/route_ejs.js
+++ public/Express/route_ejs.js
@@ -6,11 +6,36 @@ const router = express.Router();
 app.set('view engine', 'ejs');
 // must add folder that name views 
 
+app.use('/assets',express.static('assets'));
+
+
+
 // send reel data and parametre to profile  
 app.get('/profile/:name',function(req,res){  
     
     var data ={ age: 29, job: 'nothing intresting',hobbies:['Soccer','Swiming','Tenis']};
         res.render('profile',{get_param:req.params.name,data:data});
 });
+app.get('/',function(req,res){  
+    
+        res.render('index');
+});
+
+app.get('/',function(req,res){  
+    
+        res.render('index');
+});
+app.get('/contact',function(req,res){  
+    
+        res.render('Conatct');
+});
+app.get('/profile',function(req,res){  
+    
+        res.render('profile1');
+});
+
+//midleware
+
+// app.use('/assets',express.static(path.join(__dirname, 'public/assests')));
 
 app.listen('3000');
